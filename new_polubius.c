6ddf023ff93f3b29177ded18894eed3a02dc3647
S3thBr0wn <31544947+S3thBr0wn@users.noreply.github.com>
2019-04-10 08:37:58 -0700

    Add files via upload

---
+++ new_polubius.c
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void polybiusCipher(string s) {
+	int row, col;
+	
+	for (int i = 0; s[i]; i++) {
+		
+		row = ceil((s[i] - 'a') / 5) + 1;
+		
+		col = ((s[i] - 'a') %  5) + 1;
+		
+		if (s[i] == 'k') {
+			row = row - 1;
+			col = 5 - col + 1;
+		}
+		
+		else if (s[i] >= 'j') {
+			if (col == 1) {
+				col = 6;
+				row = row - 1;
+			}
+			col = col - 1;
+		}
+		cout << row << col;
+	}
+	cout << endl;
+}
+
+	int main() {
+		string s = "sdb";
+		polybiusCipher(s);
+		return 0;
+	}
